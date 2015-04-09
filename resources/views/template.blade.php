7111d4a8370d0f500ec9c5f3010d133542c26141
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2015-04-09 16:28:32 +0700

    Change link to source on github

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -16,7 +16,7 @@
         You're reading
         <div class="name">&#12300; <a href="{{ url('/') }}">The Voices Made Me Write It In PHP</a> &#12301;</div>
         a compendium of Rizqi&rsquo;s misadventures in design &amp; coding.
-        <div class="menu"><a href="{{ url('archive') }}">Archives</a> &bull; <a href="http://github.com/rizqidjamaluddin">Github</a></div>
+        <div class="menu"><a href="{{ url('archive') }}">Archives</a> &bull; <a href="http://github.com/rizqidjamaluddin/tabard">Source on Github</a></div>
     </div>
 
     @yield('content', '')
