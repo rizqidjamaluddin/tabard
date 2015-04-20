6d82d9e55dc67247c69f0590eaac6e8bb2143957
Rizqi Djamaluddin <rizqi@rizqi.web.id>
2015-04-20 20:09:58 +0700

    Add viewport meta tag

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
     <link rel="stylesheet" href="{{ asset('css/prism.css') }}"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> @yield('title', '') The Voices Made Me Write It In PHP</title>
 </head>
 <body>
